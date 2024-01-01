1b241ae26eade9731839740ba28b8f713115984f
avafox <avafoxemail@gmail.com>
2024-01-01 13:54:30 -0700

    Define player peices

--- ticktack.py
+++ ticktack.py
@@ -1,4 +1,4 @@
-# Let player one choose x or o
+# Let player one choose X or O
 options = ['X', 'O']
 message = "Player 1, choose X or O"
 
@@ -7,9 +7,10 @@ while True:
     if choice in options:
         break
 
+# Define players
 player_1 = choice
 for option in options:
-    if not choice:
+    if option != choice:
         player_2 = option
 
 print(f"Player 1: {player_1} \n Player 2: {player_2}")
